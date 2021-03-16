e5e688c51f262bd870fcacad0e262dfcea1b47f8
lamtran21 <tran_l7@denison.edu>
2021-03-16 03:12:38 -0400

    more code in data_aggregation.py

--- data_aggregation.py
+++ data_aggregation.py
@@ -3,9 +3,7 @@ import os
 import numpy as np
 from sklearn import preprocessing
 
-# todo: remove Nan => normalize => group by (mean for ratings, mode for demographics) => join 3 dfs
-
-# todo: xy in landmarks are strings. Needs to convert to int. (will do later)
+# todo: join demographic, attributes and landmarks. Then scale the landmarks (in str rn) (in landmarks)
 
 
 def create_landmarks_df(path):
@@ -44,15 +42,43 @@ def create_attributes_df(path):
     """
     A program that reads in an excel file that contains attribute scores for 2222 people and turns it into
     a pandas data frame. The program only reads in the primary key column (filename), secondary key column
-    (Image #), attractiveness ratings and unattractiveness ratings.
+    (Image #), attractiveness ratings and unattractiveness ratings. It then outputs the primary key column
+    (filename), secondary key column (Image #), sum attractive ratings (to be combined with other dataset),
+    sum unattractive ratings, and count (to be used to calculate the average later on) (count = how many ratings
+    this person gets, as I have dropped NAs and some do not have 15 ratings anymore)
 
     :param path: path to the excel file
     :return: a data frame that contains attribute scores for 2222 people.
     """
+
+    # Read in files
     attributes = pd.read_excel(path, na_values=['NaN'], usecols="A,B,W,AD")
-    # print(attributes.head())
-    # print(attributes.shape)
-    return attributes
+
+    # Drop NA values
+    attributes = attributes.dropna()  # drop 58 ratings
+    # print(len(set(attributes.iloc[:, 0])))  # no one among 2222 people is completely removed from data
+
+    # Min-max normalize the attractive ratings
+    attributes['attractive'] = (attributes['attractive'] - attributes['attractive'].min()) / \
+                               (attributes['attractive'].max() - attributes['attractive'].min())
+    attributes['unattractive'] = (attributes['unattractive'] - attributes['unattractive'].min()) / \
+                                 (attributes['unattractive'].max() - attributes['unattractive'].min())
+    print(attributes['attractive'].head(), attributes['attractive'].min(), attributes['attractive'].max())
+
+    # Group by and summarize by person
+    agg_attributes = attributes.groupby(['Filename', 'Image #'], as_index=False).agg({
+                                                                                    'attractive': ['sum', 'count'],
+                                                                                    'unattractive': 'sum'
+                                                                                            })
+    # drop second-level column names that were created by group_by (eg: sum, count)
+    agg_attributes.columns = agg_attributes.columns.droplevel(1)
+
+    # rename the columns as we have 2 columns named attractive
+    agg_attributes.columns = ['Filename', 'Image #', 'attractive', 'count', 'unattractive']
+
+    # Comment: some images have 16 ratings which is weird because it was claimed each has 15 before removing NAs
+    print(agg_attributes['count'].value_counts())
+    return agg_attributes
 
 
 def create_demographic_df(path):
@@ -65,29 +91,39 @@ def create_demographic_df(path):
     :return: a data frame that contains demographic information for 2222 people.
     """
     demographic = pd.read_excel(path, na_values=['NaN'], usecols="A,B,C,D,O,S")
-    # print(demographic.head())
-    # print(demographic.shape)
-    # look into correlation between attractiveness - unattractiveness before deciding if use unattractiveness
-    return demographic
 
+    # Drop NA values
+    demographic = demographic.dropna()  # remove 34 observations
 
-def normalize_ratings(df, rating_col):
-    x_array = np.array(df[rating_col])
-    normalized_x = preprocessing.normalize([x_array])
-    print(normalized_x)
-    print(max(normalized_x))
-    print(min(normalized_x))
-    print(np.average(normalized_x))
+    # Min-max normalize attractive ratings
+    demographic['Attractive'] = (demographic['Attractive'] - demographic['Attractive'].min()) /\
+                                (demographic['Attractive'].max() - demographic['Attractive'].min())
+
+    # Group by and summarize by person
+    agg_demographic = demographic.groupby(['Filename', 'Image #'], as_index=False).agg({
+                                                                'Attractive': ['sum', 'count'],
+                                                                'Gender': (lambda x: pd.Series.mode(x).values[0]),
+                                                                'Race': (lambda x: pd.Series.mode(x).values[0]),
+                                                                'Age': (lambda x: pd.Series.mode(x).values[0]),
+                                                                })
+
+    # drop second-level column names that were created by group_by (eg: sum, count)
+    agg_demographic.columns = agg_demographic.columns.droplevel(1)
+
+    # rename the columns as we have 2 columns named attractive
+    agg_demographic.columns = ['Filename', 'Image #', 'attractive', 'count', 'gender', 'race', 'age']
+
+    print(agg_demographic.head())
+    return agg_demographic
 
 
 def main():
     path = "L:/Spring 2021/DA 401/10k US Faces Data/"
     # landmarks = create_landmarks_df(path + "annotations/Face Annotations/Images and Annotations/")
-    attributes = create_attributes_df(path + "2kattributes/Full Attribute Scores/psychology attributes/"
-                                             "psychology-attributes.xlsx")
-    # demographic = create_demographic_df(path + "2kattributes/Full Attribute Scores/demographic & others labels/"
-    #                                           "demographic-others-labels.xlsx")
-    # normalize_ratings(attributes, "attractive") <- needs to remove NaN from df
+    # attributes = create_attributes_df(path + "2kattributes/Full Attribute Scores/psychology attributes/"
+    #                                         "psychology-attributes.xlsx")
+    demographic = create_demographic_df(path + "2kattributes/Full Attribute Scores/demographic & others labels/"
+                                               "demographic-others-labels.xlsx")
 
 
 main()
